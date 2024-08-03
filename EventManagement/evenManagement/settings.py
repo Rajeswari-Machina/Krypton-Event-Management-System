2a8a5298cda1f0ffa40aa6a4eb93bf9c7a9bcd1e
Rajeswari Machina <151704782+Rajeswari-Machina@users.noreply.github.com>
2024-08-03 22:00:45 +0530

    Update settings.py

--- EventManagement/evenManagement/settings.py
+++ EventManagement/evenManagement/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-w94o00)5_s@slur^507wdauqsul4)hjc9vglfyould1kbspzvu'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.vercel.app','now.sh']
 
 
 
@@ -119,6 +119,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build','static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
